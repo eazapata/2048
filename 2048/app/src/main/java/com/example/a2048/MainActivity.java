50b747c6f5287e4c6f71df51ca885171e26f19f7
Eduardo Antonio Zapata Valero <eazapatavalero@gmail.com>
2021-03-01 20:50:50 +0100

    !

---
+++ 2048/app/src/main/java/com/example/a2048/MainActivity.java
@@ -0,0 +1,198 @@
+package com.example.a2048;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.a2048.DataBase.DataBaseHelper;
+import com.example.a2048.DataBase.Score;
+import com.example.a2048.game.Game;
+
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, SwipeCallback, View.OnClickListener {
+
+    private ImageView[][] imageViews;
+    private int[][] textViewValues;
+    private GridLayout grid;
+    private SwipeListener swipeListener;
+    private TextView scoreTextView;
+    private int actualScore;
+    private int[][] previousValues = new int[4][4];
+    private Button undo;
+    private Button newGame;
+    private Game game;
+    private TextView playerName;
+    private DataBaseHelper dataBaseHelper;
+    private String player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        game = new Game();
+        swipeListener = new SwipeListener(this, this);
+        scoreTextView = (TextView) findViewById(R.id.score_field);
+        playerName = (TextView) findViewById(R.id.player_name_field);
+
+        scoreTextView.setText("0");
+        dataBaseHelper = new DataBaseHelper(this, "score", null, 1);
+        Intent intent = getIntent();
+
+        Bundle extras = intent.getExtras();
+        player = extras.getString("PLAYER KEY");
+        playerName.setText(player);
+        this.undo = (Button) findViewById(R.id.undo);
+        this.undo.setOnClickListener(this);
+        this.newGame = (Button) findViewById(R.id.new_game);
+        this.newGame.setOnClickListener(this);
+        this.textViewValues = new int[4][4];
+        this.imageViews = fillArray();
+        this.grid = (GridLayout) findViewById(R.id.grid);
+        this.grid.setOnTouchListener(this);
+        setRandomNumber();
+    }
+
+    @Override
+    public void onSwipe(Direction direction) {
+        copyArray();
+        switch (direction) {
+            case UP:
+                this.actualScore = game.up(imageViews, textViewValues, this.actualScore);
+                setRandomNumber();
+                break;
+            case DOWN:
+                this.actualScore = game.down(imageViews, textViewValues, this.actualScore);
+                setRandomNumber();
+                break;
+            case LEFT:
+                this.actualScore = game.left(imageViews, textViewValues, this.actualScore);
+                setRandomNumber();
+                break;
+            case RIGHT:
+                this.actualScore = game.right(imageViews, textViewValues, this.actualScore);
+                setRandomNumber();
+                break;
+            default:
+                System.out.println("Wrong direction");
+                break;
+        }
+        this.scoreTextView.setText(String.valueOf(this.actualScore));
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        swipeListener.onTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (((Button) v).getText() == this.undo.getText()) {
+            undoMovement();
+            System.out.println("Works");
+        } else if (((Button) v).getText() == this.newGame.getText()) {
+            newGame();
+        }
+    }
+
+    private void copyArray() {
+        for (int i = 0; i < textViewValues.length; i++) {
+            for (int j = 0; j < textViewValues[i].length; j++) {
+                previousValues[i][j] = textViewValues[i][j];
+            }
+        }
+    }
+
+    private ImageView[][] fillArray() {
+        ImageView textView1 = (ImageView) findViewById(R.id.one);
+        ImageView textView2 = (ImageView) findViewById(R.id.two);
+        ImageView textView3 = (ImageView) findViewById(R.id.three);
+        ImageView textView4 = (ImageView) findViewById(R.id.four);
+        ImageView textView5 = (ImageView) findViewById(R.id.five);
+        ImageView textView6 = (ImageView) findViewById(R.id.six);
+        ImageView textView7 = (ImageView) findViewById(R.id.seven);
+        ImageView textView8 = (ImageView) findViewById(R.id.eight);
+        ImageView textView9 = (ImageView) findViewById(R.id.nine);
+        ImageView textView10 = (ImageView) findViewById(R.id.ten);
+        ImageView textView11 = (ImageView) findViewById(R.id.eleven);
+        ImageView textView12 = (ImageView) findViewById(R.id.twelve);
+        ImageView textView13 = (ImageView) findViewById(R.id.thirteen);
+        ImageView textView14 = (ImageView) findViewById(R.id.fourteen);
+        ImageView textView15 = (ImageView) findViewById(R.id.fifteen);
+        ImageView textView16 = (ImageView) findViewById(R.id.sixteen);
+        ImageView[][] arrayText = {
+                {textView1, textView2, textView3, textView4},
+                {textView5, textView6, textView7, textView8},
+                {textView9, textView10, textView11, textView12},
+                {textView13, textView14, textView15, textView16}};
+        return arrayText;
+    }
+
+    private void newGame() {
+        this.textViewValues = null;
+        this.textViewValues = new int[4][4];
+        this.actualScore = 0;
+        this.scoreTextView.setText(String.valueOf(0));
+        resetImages();
+        setRandomNumber();
+    }
+
+    private void resetImages() {
+        for (int i = 0; i < imageViews.length; i++) {
+            for (int j = 0; j < imageViews[i].length; j++) {
+                imageViews[i][j].setImageDrawable(null);
+            }
+        }
+    }
+
+    public void setRandomNumber() {
+        if (game.checkEndgame(this.textViewValues)) {
+            Score score = new Score();
+            score.setPlayer(player);
+            score.setPlayerScore(this.actualScore);
+            dataBaseHelper.insertScore(score);
+            finish();
+        } else {
+            Random random = new Random();
+            int pos1 = random.nextInt(4);
+            int pos2 = random.nextInt(4);
+            int[] numbers = {2, 4};
+            int value = numbers[random.nextInt(2)];
+            while (this.textViewValues[pos1][pos2] != 0) {
+                pos1 = random.nextInt(4);
+                pos2 = random.nextInt(4);
+
+            }
+            if (value == 2) {
+                this.imageViews[pos1][pos2].setImageResource(R.drawable.two_img);
+            } else {
+                this.imageViews[pos1][pos2].setImageResource(R.drawable.four);
+            }
+            this.textViewValues[pos1][pos2] = value;
+
+        }
+    }
+
+    private void undoMovement() {
+        resetImages();
+        for (int i = 0; i < textViewValues.length; i++) {
+            for (int j = 0; j < textViewValues[i].length; j++) {
+                textViewValues[i][j] = previousValues[i][j];
+            }
+        }
+        game.setImage(imageViews, textViewValues);
+
+    }
+
+}
---
+++ 2048/app/src/main/java/com/example/a2048/listView/ListMenu.java
@@ -0,0 +1,68 @@
+package com.example.a2048.listView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.a2048.DataBase.ScoresActivity;
+import com.example.a2048.MainActivity;
+import com.example.a2048.R;
+import com.example.a2048.listView.Item;
+import com.example.a2048.listView.ListViewAdapter;
+
+import java.util.ArrayList;
+
+public class ListMenu extends AppCompatActivity  implements AdapterView.OnItemClickListener{
+    private ListView listView;
+    private ListViewAdapter adapter;
+    private ArrayList<Item> activities;
+    private EditText editTextName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_menu);
+
+        editTextName = (EditText) findViewById(R.id.player_name_menu);
+        listView = findViewById(R.id.listView);
+        listView.setOnItemClickListener(this);
+        this.getActivities();
+        this.adapter = new ListViewAdapter(this,activities);
+        listView.setAdapter(this.adapter);
+
+
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent;
+        switch (position){
+            case 0:
+                String player_name = editTextName.getText().toString();
+                intent = new Intent(this, MainActivity.class);
+                intent.putExtra("PLAYER KEY",player_name);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(this, ScoresActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                Log.d("Switch fail","Activity not found.");
+        }
+    }
+
+    private void getActivities(){
+        this.activities = new ArrayList<>();
+        this.activities.add(new Item("Play"));
+        this.activities.add(new Item("Manage Scores"));
+    }
+}
